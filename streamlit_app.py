18f01b47eff2df682696daa4ad43ecd98b187ec9
sbluen <sbluen153@yahoo.com>
2025-02-14 00:02:14 -0800

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,32 @@
+# Import python packages
+import streamlit as st
+from snowflake.snowpark.context import get_active_session
+from snowflake.snowpark.functions import col
+
+# Write directly to the app
+st.title(":cup_with_straw: Customize your smoothie! :cup_with_straw:")
+st.write(
+    """Choose the fruits you want in your custom smoothie!"""
+)
+
+name_on_smoothie = st.text_input("Name on Smoothie:")
+
+session = get_active_session()
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+#st.dataframe(data=my_dataframe, use_container_width=True)
+
+
+ingredients_list = st.multiselect("Choose up to 5 ingredients:", 
+                                  my_dataframe, 
+                                  max_selections=5)
+
+ingredients_string = ""
+for fruit in ingredients_list:
+    ingredients_string += fruit + " "
+
+my_insert_sql = """INSERT INTO smoothies.public.orders(ingredients, name_on_order) VALUES (?, ?)"""
+
+time_to_submit = st.button("Submit Order")
+if time_to_submit:
+    session.sql(my_insert_sql, [ingredients_string, name_on_smoothie]).collect()
+    st.success("Your smoothie is ordered, %s" % name_on_smoothie, icon="✅")
