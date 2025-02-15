601f8f4978aad4c5bf76d194944e74ffd3ec3174
sbluen <sbluen153@yahoo.com>
2025-02-14 18:43:28 -0800

    Update streamlit_app.py
    use pandas to write the search string

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
+import pandas as pd
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize your smoothie! :cup_with_straw:")
@@ -24,6 +25,9 @@ ingredients_list = st.multiselect("Choose up to 5 ingredients:",
 ingredients_string = ""
 for fruit in ingredients_list:
     ingredients_string += fruit + " "
+    search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+    st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+    
     st.subheader(fruit + " Nutrition information")
     smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit)
     st_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True, col("search_on"))
