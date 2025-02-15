e5215f013b1506261fb4bc2d4dc29ec3aa7ac8a7
sbluen <sbluen153@yahoo.com>
2025-02-14 18:27:29 -0800

    Update streamlit_app.py
    show the dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,3 +34,4 @@ if time_to_submit:
 
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 st.text(smoothiefroot_response)
+st_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
