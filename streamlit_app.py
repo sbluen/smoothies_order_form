be779e12ffd3dcf6faa733cc5bd7aee5de667892
sbluen <sbluen153@yahoo.com>
2025-02-14 18:51:57 -0800

    Update streamlit_app.py
    convert dataframe to pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,6 +25,7 @@ ingredients_list = st.multiselect("Choose up to 5 ingredients:",
 ingredients_string = ""
 for fruit in ingredients_list:
     ingredients_string += fruit + " "
+    pd_df = my_dataframe.to_pandas()
     search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
     st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
     
