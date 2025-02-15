9d4b552f6ff5824e9b2c7a1dccb5125ff291b3af
sbluen <sbluen153@yahoo.com>
2025-02-14 19:28:46 -0800

    Update streamlit_app.py
    add column order

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,7 +31,7 @@ for fruit in ingredients_list:
     
     st.subheader(fruit + " Nutrition information")
     smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on)
-    fv_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+    fv_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True, column_order=[1, 4, 5])
 
     
 
