337468067935bf53a42a9279bd140ff226828ea1
sbluen <sbluen153@yahoo.com>
2025-02-15 19:08:22 -0800

    Update streamlit_app.py
    let's see if it looks better as a string

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,11 +31,9 @@ for fruit in ingredients_list:
     
     st.subheader(fruit + " Nutrition information")
     smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on)
-    #fv_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)[nutrition]
     df_smoothiefruit = pd.DataFrame(smoothiefroot_response.json())['nutrition']
-    st.dataframe(data=df_smoothiefruit, use_container_width=True)    #to display it
-
-    
+    #st.dataframe(data=df_smoothiefruit, use_container_width=True)
+    st.write(str(df_smoothiefruit))
 
 my_insert_sql = """INSERT INTO smoothies.public.orders(ingredients, name_on_order) VALUES (?, ?)"""
 
