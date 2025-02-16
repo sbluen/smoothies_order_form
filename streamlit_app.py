1867aa4d1c23f8c6c2246a752688465b55203741
sbluen <sbluen153@yahoo.com>
2025-02-15 19:05:30 -0800

    Update streamlit_app.py
    fix parameter used in wrong function

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,8 +32,8 @@ for fruit in ingredients_list:
     st.subheader(fruit + " Nutrition information")
     smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on)
     #fv_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)[nutrition]
-    df_smoothiefruit = pd.DataFrame(data=smoothiefroot_response.json(), use_container_width=True)[nutrition]
-    st.dataframe(data=df_smoothiefruit)    #to display it
+    df_smoothiefruit = pd.DataFrame(smoothiefroot_response.json())[nutrition]
+    st.dataframe(data=df_smoothiefruit, use_container_width=True)    #to display it
 
     
 
